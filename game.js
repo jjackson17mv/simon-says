06c5d94a9d255313d481f3a9079824843ddc6602
josh jackson <47463238+jjackson17mv@users.noreply.github.com>
2024-02-26 18:33:46 -0800

    Add files via upload
    added sound

--- game.js
+++ game.js
@@ -1,14 +1,44 @@
 var buttonColors = ["red", "blue", "green", "yellow"];
 var gamePattern = [];
 
+var userClickedPattern = [];
+
+$(".btn").click(function() {
+  var userChosenColor = $(this).attr("id");
+
+  userClickedPattern.push(userChosenColor);
+
+  console.log(userClickedPattern);
+
+  playSound(userChosenColor);
+});
+
+
+
+
+
+
+
 function nextSequence() {
   var randomNumber = Math.floor(Math.random() * 4);
   var randomChosenColor = buttonColors[randomNumber];
 
   gamePattern.push(randomChosenColor);
+
+  $("#" + randomChosenColor)
+    .fadeIn(100)
+    .fadeOut(100)
+    .fadeIn(100);
+
+  var audio = new Audio("sounds/" + randomChosenColor + ".mp3");
+  audio.play();
+
+  playSound(userChosenColor)
 }
 
-$("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100)
+function playSound(name) {
+  
 
-var audio = new Audio("sounds/" + randomChosenColor + ".mp3")
-audio.play()
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
