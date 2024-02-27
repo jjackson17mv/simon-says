4974b26418d1e7b5f286f1ad56e70ac375871e4a
josh jackson <47463238+jjackson17mv@users.noreply.github.com>
2024-02-26 16:54:01 -0800

    Add files via upload
    added fadeIn and fadeOut

--- game.js
+++ game.js
@@ -1,1 +1,11 @@
-alert("this works")
+var buttonColors = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+
+function nextSequence() {
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColor = buttonColors[randomNumber];
+
+  gamePattern.push(randomChosenColor);
+}
+
+$("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100)
